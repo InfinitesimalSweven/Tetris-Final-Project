c2f5236d3e7630329089fd3583bf49a83980f719
InfinitesimalSweven <natgarg132@gmail.com>
2026-04-25 22:11:19 -0400

    Added Tables

--- piece.c
+++ piece.c
@@ -2,9 +2,7 @@
 #include "blockOffsets.h"
 #include "board.h"
 
-typedef enum {
-    EMPTY, LIGHT_BLUE, DARK_BLUE, ORANGE, YELLOW, GREEN, RED, MAGENTA
-} BlockColor;
+
 typedef enum {
     EMPTY, I_BLOCK, J_BLOCK, L_BLOCK, O_BLOCK, S_BLOCK, Z_BLOCK, T_BLOCK
 } BlockType;
@@ -15,39 +13,38 @@ typedef enum {
 typedef struct {
     int blockCords[4][2]; //Origin cord defined at [0][2]
     int rotation; //Rotation states-> 0 origin, 1 is 90 degrees, 2 is 180 degrees, and 3 is 270 degrees 
-    BlockType letter;
-    ColorType color;
+    BlockType blockType;
 } Piece
 
 //spawns piece at appropriate location. (We choose coordinate system (0,0) as top-left)
 //We use a 40x10 board. Why? Idk the tetris requirements told me to. We really only need 10x24...
-Piece createPiece(BlockType letter, ColorType color){
-    Piece tetrisBlock = {.rotation = 0, .letter = letter, .color = color}
+Piece createPiece(BlockType blockType){
+    Piece tetrisBlock = {.rotation = 0, .blockType = blockType}
     memcpy(tetrisBlock.blockCords, spawnPos[letter], sizeof(tetrisBlock.blockCords));
     return tetrisBlock;
 }
 
-bool checkCollision(Piece TetrisBlock, int dx, int dy, Board* Board) {
+int checkCollision(Piece TetrisBlock, int dx, int dy, Board* Board) {
     for (int i = 0; i < 4; i++) {
         TetrisBlock.blockCords[i][0] += dx;
         TetrisBlock.blockCords[i][1] += dy;
 
         if (TetrisBlock.blockCords[i][0] >= 10 ||
             TetrisBlock.blockCords[i][1] >= 40){
-                return true;
+                return 1;
         }
 
 
         if (TetrisBlock.blockCords[i][0] < 0 ||
             TetrisBlock.blockCords[i][1] < 0){
-                return true;
+                return 1;
         }
 
         if (Board->grid[TetrisBlock.blockCords[i][1]][TetrisBlock.blockCords[i][0]] > 0){
-                return true;
+                return 1;
             }
         }
-    return false;
+    return 0;
 }
 
 
@@ -58,10 +55,18 @@ void translatePiece(Piece* tetrisBlock, int dx, int dy) {
     }
 }
 
-void transCollision(Piece* TetrisBlock, int dx, int dy, Board* Board){
+void rotatePiece(Piece* tetrisBlock, int dx, int dy){
+    
+}
 
-    if (!checkCollision(*TetrisBlock, dx, dy, Board))
+int transCollision(Piece* TetrisBlock, int dx, int dy, Board* Board){
+
+    if (!checkCollision(*TetrisBlock, dx, dy, Board)){
         translatePiece(TetrisBlock, dx, dy);
+        return 1;
+    }
+    return 0;
+
 
 }
 
@@ -74,14 +79,23 @@ void rotCollision(Piece* TetrisBlock, int dx, int dy, Board* Board){
 //its ok readable code is more important methinks, maybe ill comment a binTree version
 //and ask kyle what he thinks
 
-#define collisionInusrance
-int hardDropPiece(Piece* TetrisBlock, Board* Board){
-
-    for(int i = 0; i < 25; i++){
-        checkCollision(TetrisBlock, 0, -i, Board);
+void hardDropPiece(Piece* TetrisBlock, Board* Board){
+    int i = 0;
+    for(i = 0; i < 25; i++){
+        if (checkCollision(*TetrisBlock, 0, -i, Board))
+            break;
     }
 
-    return i;
+    transCollision(TetrisBlock, 0, -(i-1), Board);
+}
+
+//Places piece on to the board
+int placePiece(Piece* TetrisBlock, Board* Board){
+
+    for (int i = 0; i < 4; i++){
+        Board->grid[TetrisBlock->blockCords[i][0]][TetrisBlock->blockCords[i][1]] = TetrisBlock->blockType;
+    }
+
 }
 
 
