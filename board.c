56074d80a5570b9ab3db6e7c4785d7ee6b462f43
Kyle Zhang <kyle.zhang@cooper.edu>
2026-05-04 22:42:07 -0400

    stuff

--- board.c
+++ board.c
@@ -4,9 +4,6 @@
 #define COLS 10
 #define CELL 32
 
-
-//This file will be expanded to create checker files.
-
 //Originally the plan was the grid was going to contain 1's for filled in and 0 for not filled in.
 //However, it might be better to assign each color a number, and if number > 0 its filled in.
 //Define each color and assign it to a number. Then we can use colors as numbers!
@@ -20,7 +17,6 @@ Board createBoard(int score, int level){
 }
 
 int checkAndClearLine(Board* TetrisBoard, int yLow, int yHigh){
-
     int isFull;
     int linesCleared = 0;
     for (int y = yHigh; y >= yLow; y--){
